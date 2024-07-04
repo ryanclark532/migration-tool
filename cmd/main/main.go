71604f1f7945da96f327f5bf1124d140168f32ed
Ryan Clark <clark0859@gmail.com>
2024-07-04 20:24:18 +1000

    ?

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,45 +1,8 @@
 package main
 
 import (
-	"ryanclark532/migration-tool/internal/differ"
-	"ryanclark532/migration-tool/internal/migrate"
-	"ryanclark532/migration-tool/internal/sqlserver"
+    tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	//Get State of the Database before migration
-	server := &sqlserver.SqlServer{
-		Server:   "localhost",
-		Port:     1433,
-		User:     "sa",
-		Password: "yourStrong(!)Password",
-		Database: "master",
-	}
-
-	err := server.Connect()
-	if err != nil {
-		panic(err)
-	}
-
-	version,err := server.GetLatestVersion()
-	if err != nil{
-		panic(err)
-	}
-
-	original, err := server.GetDatabaseState()
-	if err != nil {
-		panic(err)
-	}
-
-	migrate.DoMigration(server.Conn)
-
-	post, err:= server.GetDatabaseState()
-	if err != nil {
-		panic(err)
-	}
-
-
-	differ.GetDiff(original, post, version)
-
-	server.Close()
 }
